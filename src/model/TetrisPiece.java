963474d2502d4dd0a85cb713095797af51f1043d
ddxbugs <ddxbugs@proton.me>
2022-11-15 23:46:37 -0800

    model/TetrisPiece.java documentation minor correction punctuation

--- src/model/TetrisPiece.java
+++ src/model/TetrisPiece.java
@@ -42,21 +42,21 @@ enum TetrisPiece {
 			new Point (1, 1),
 			new Point (2, 1)),
 	
-	/** The 'S' TetrisPiece. */
+	/** The 'S' TetrisPiece */
     S(3, 2, Block.S,
     		new Point (1, 2), 
 			new Point (2, 2), 
 			new Point (0, 1), 
 			new Point (1, 1)),
     
-    /** The 'T' TetrisPiece. */
+    /** The 'T' TetrisPiece */
     T(3, 2, Block.T,
     		new Point (1, 2), 
     		new Point (0, 1), 
     		new Point (1, 1), 
     		new Point (2, 1)),
 
-    /** The 'Z' TetrisPiece. */
+    /** The 'Z' TetrisPiece */
     Z(3, 2, Block.Z,
     		new Point (0, 2), 
     		new Point (1, 2), 
