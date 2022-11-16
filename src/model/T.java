650087c242754146ccdd0292aa149794d83ba060
ddxbugs <ddxbugs@proton.me>
2022-11-15 23:43:20 -0800

    src/model/T.java

---
+++ src/model/T.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package model;
+
+/**
+ * @author User
+ *
+ */
+public class T extends MutableTetrisPiece {
+
+	/**
+	 * 
+	 */
+	public T() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected int getHeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	protected int getWidth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
