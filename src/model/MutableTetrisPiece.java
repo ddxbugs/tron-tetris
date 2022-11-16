dc98d10112f48d4a81c0a0f9e062eddd102b846e
ddxbugs <ddxbugs@proton.me>
2022-11-15 23:44:46 -0800

    initial commit: model/MutableTetrisPiece.java abstract object

---
+++ src/model/MutableTetrisPiece.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package model;
+
+/**
+ * @author User
+ *
+ */
+abstract class MutableTetrisPiece {
+
+	/**
+	 * 
+	 */
+	public MutableTetrisPiece() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	protected abstract int getHeight();
+	protected abstract int getWidth();
+}
