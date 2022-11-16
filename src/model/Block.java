fe31228d490df99391f8bdc193210bcd8dd206d8
ddxbugs <ddxbugs@proton.me>
2022-11-15 21:51:06 -0800

    src/model block enum and point class initial commit

---
+++ src/model/Block.java
@@ -0,0 +1,27 @@
+/**
+ * Block enums
+ */
+package model;
+
+/**
+ * @author ddxbugs@github.com
+ *	The different type of blocks that can be stored in a Board's grid.
+ */
+enum Block {
+	/** Empty */
+	EMPTY,
+	/** 'I' block */
+	I,
+	/** 'J' block */
+	J,
+	/** 'L' block */
+	L,
+	/** 2x2 block */
+	O,
+	/** 'S' block */
+	S,
+	/** 'T' block */
+	T,
+	/** 'Z' block */
+	Z;
+}
---
+++ src/model/Point.java
@@ -0,0 +1,88 @@
+/**
+ * Point.java
+ */
+package model;
+
+import java.util.Objects;
+
+/**
+ * Represent a Point with x and y coordinates.
+ * Point objects are immutable.
+ * (java.awt.Point are mutable)
+ * @author ddxbugs@github.com
+ *
+ */
+final class Point {
+	/** The tetris block X coordinate */
+	private final int myX;
+	/** The tetris block Y coordinate */
+	private final int myY;
+	
+	/**
+	 * Constructor for Point using given coordinate params
+	 * @param theX
+	 * @param theY
+	 */
+	protected Point(final int theX, final int theY) {
+		// TODO Auto-generated constructor stub
+		myX = theX;
+		myY = theY;
+	}
+	
+	/**
+	 * Block X coord getter 
+	 * @return the tetris block X coord
+	 */
+	protected int getX() {
+		return myX;
+	}
+	
+	/**
+	 * Block Y coord gettter
+	 * @return the tetris block Y coord
+	 */
+	protected int getY() {
+		return myY;
+	}
+	
+	/**
+	 * Transforms the block X,Y points to new X,Y points
+	 * @param theX the domain transformation
+	 * @param theY the range transformation
+	 * @return the new block X,Y transformation
+	 */
+	protected Point transform(final int theX, final int theY) {
+		return new Point(myX + theX, myY + theY);
+	}
+	
+	/**
+	 * Overrides Object inherited equals method
+	 * @return the result of the comparison operation
+	 */
+	@Override
+	public boolean equals(final Object theOther) {
+		boolean result = false;
+		if (theOther == this) result = true;
+		else if (theOther != null && theOther.getClass() == getClass()) {
+			final Point p = (Point) theOther;
+			result = myX == p.myX && myY == p.myY;
+		}
+		return result;			
+	}
+	
+	/**
+	 * Return the block X,Y object hash value 
+	 */
+	@Override
+	public int hashCode() {
+		return Objects.hash(myX, myY);
+	}
+	
+	/**
+	 * Return the block X,Y string format
+	 */
+	@Override
+	public String toString() {
+		return String.format("(%d, %d)", myX, myY);
+	}
+}
