7c188ff1ca9385440264afe78105722a5ee28ca8
ddxbugs <ddxbugs@proton.me>
2022-11-18 16:51:13 -0800

    model/T.java extends TetrisPiece calls default start constructor

--- src/model/T.java
+++ src/model/T.java
@@ -5,12 +5,27 @@ package model;
 
 /**
  * T class extends TetrisPiece
- * TODO: implement final class 
+ * TODO: implement final class
+ * TODO: T class implements special function/methods 
  * Example: T, L, S, O, Z TetrisPiece extends abstract TetrisPiece
  * @author User
  *
  */
 public class T extends TetrisPiece {
+	/** 
+	 * Default T constructor
+	 * T BlockType extends TetrisPiece
+	 */
+	public T() {
+		super(BlockType.T, new Point(0, 0), Rotation.START);
+	}
 	
+	/**
+	 * Moving T constructor
+	 * T BlockType extends TetrisPiece
+	 */
+	public T(final Point thePoint, final Rotation theRotation) {
+		super(BlockType.T, thePoint, theRotation);
+	}
 }
 
