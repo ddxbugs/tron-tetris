3b08a28db4b2f2a440e3816fe9416ff887825b2c
ddxbugs <ddxbugs@proton.me>
2022-11-19 23:45:01 -0800

    Initial commit: model/Z additional tetris piece final class object

---
+++ src/model/Z.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package model;
+
+/**
+ * @author User
+ *
+ */
+public class Z extends TetrisPiece {
+
+	/**
+	 * @param theTetrisPiece
+	 * @param thePosition
+	 * @param theRotation
+	 */
+	public Z(BlockType theTetrisPiece, Point thePosition, Rotation theRotation) {
+		super(theTetrisPiece, thePosition, theRotation);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param theTetrisPiece
+	 * @param thePosition
+	 */
+	public Z(BlockType theTetrisPiece, Point thePosition) {
+		super(theTetrisPiece, thePosition);
+		// TODO Auto-generated constructor stub
+	}
+
+}
