21a34ff60ffd4ed9cf3bd4bff7201c8eb3a813d0
ddxbugs <ddxbugs@proton.me>
2022-11-19 23:43:11 -0800

    Initial commit: view/BoardView tetris board graphics2D representation

---
+++ src/view/BoardView.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package view;
+
+import javax.swing.JPanel;
+
+/**
+ * @author User
+ *
+ */
+public class BoardView extends JPanel {
+
+	/**
+	 * Generated serialVersionUID
+	 */
+	private static final long serialVersionUID = 8563705674683776847L;
+	
+}
