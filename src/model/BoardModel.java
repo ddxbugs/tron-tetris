cfc70459a6cdcef90833313ad304386a5bc83adc
ddxbugs <ddxbugs@proton.me>
2022-11-22 15:57:58 -0800

    model/BoardModel drops and moves pieces down with collision detect

--- src/model/BoardModel.java
+++ src/model/BoardModel.java
@@ -9,7 +9,8 @@ import java.util.List;
 import java.util.Objects;
 
 /**
- * @author User
+ * BoardModel handles tetris piece movement and logic
+ * @author ddxbugs
  *
  */
 public class BoardModel {
@@ -25,7 +26,7 @@ public class BoardModel {
 	/** Tetris game player default level 0 */
 	private Player myPlayer;
 	/** The current tetris piece in play */
-	private TetrisPiece myTetrisPiece;
+	private TetrisPiece myCurrentPiece;
 	/**
 	 * Primary BoardModel constructor
 	 * @param theWidth width of game board
@@ -37,68 +38,84 @@ public class BoardModel {
 		myFrozenBlocks = new LinkedList<Block[]>();
 		myTetrisPieces = new ArrayList<TetrisPiece>();
 		myPlayer = null;
-		myTetrisPiece = null;
+		myCurrentPiece = null;
 		
 	}
-	
+	/**
+	 * Creates a new tetris game instance
+	 * Reset the game variables
+	 */
 	public void newGame() {
 		myPlayer = new Player();	// new player
+//		myPlayer = new Player(name);
 	
 		myFrozenBlocks.clear();	// clear the Tetris board
 		// TODO reset score
 		// TODO reset tetris piece preview panel
 	}
-	
 	/**
 	 * Moves the TetrisPiece left
 	 */
 	public void left() {
-		myTetrisPiece.left();
+		if (isMovable(myCurrentPiece)) {
+			myCurrentPiece.left();
+		}
 	}
-	
 	/**
 	 * Moves the TetrisPiece right
 	 */
 	public void right() {
-		myTetrisPiece.right();
-	}
-	
-	/** 
-	 * Rotate the TetrisPiece clockwise 90 degrees
-	 */
-	public void rotate() {
-		// TODO if(ImmutableTetrisPiece.O) rotate()
-		final TetrisPiece piece = myTetrisPiece.rotate();
-		final Point[] offsets = TGMRotation.getOffset(piece.getTetrisPiece(),
-				myTetrisPiece.getRotation(),
-				piece.getRotation());
-		for (final Point p : offsets) {
-			final Point offset = piece.getPosition().transform(p);
-			final TetrisPiece t = piece.setPosition(offset);
-			if (isMovable(t)) {
-				break;	// breaks if Block 't' is legal move after rotation
-			}
+		if (isMovable(myCurrentPiece)) {
+			myCurrentPiece.right();	
 		}
 	}
-	
 	/**
 	 * Move the TetrisPiece down
 	 */
 	public void down() {
 		// TODO implement freeze blocks, clear lines, update listeners
-		myTetrisPiece.down();
+		if (isMovable(myCurrentPiece)) {
+			myCurrentPiece.down();
+		} else {
+			for (final Point p : myCurrentPiece.getBoardPoints()) {
+				if (p.getX() >= 0 && p.getX() <= myWidth && p.getY() >= 0 && p.getY() <= myHeight) {
+					final Block[] blocks = myFrozenBlocks.get(p.getY());
+					blocks[p.getX()] = myCurrentPiece.getTetrisPiece().getBlock();
+				}
+			}
+			checkRows();
+		}
 	}
-	
 	/**
-	 * Call down() until TetrisPiece freezes
+	 * Drop the piece instantly to the specified location
 	 */
 	public void drop() {
 		// TODO while piece can still move down => down()
-		myTetrisPiece.down();
+		while (isMovable(myCurrentPiece) ) {
+			myCurrentPiece.down();
+		}
+	}
+	/** 
+	 * Rotate the TetrisPiece clockwise 90 degrees
+	 * Wallkicks offset TetrisPiece points' positions
+	 */
+	public void rotate() {
+		// TODO if(ImmutableTetrisPiece.O) rotate()
+		final TetrisPiece piece = myCurrentPiece.rotate();
+		final Point[] offsets = TGMRotation.getOffset(piece.getTetrisPiece(),
+				myCurrentPiece.getRotation(),
+				piece.getRotation());
+		for (final Point p : offsets) {
+			final Point offset = piece.getPosition().transform(p);
+			final TetrisPiece t = piece.setPosition(offset);
+			if (isMovable(t)) {
+				break;	// breaks if 't' is legal move after rotation, else iter next point
+			}
+		}
 	}
 	
 	/**
-	 * Checks the current piece movement logic for collision, freeze, wallkicking, boundaries
+	 * Checks the current piece movement logic for collision, freeze, boundaries
 	 * @param theTetrisPiece Current piece in play
 	 * @return Returns true if the piece is able to move in the specified direction
 	 */
@@ -116,14 +133,19 @@ public class BoardModel {
 		return isMovable;
 	}
 	
+	/**
+	 * Checks the tetris board for completed lines
+	 */
 	private void checkRows() {
-		for (final Block[] row : myFrozenBlocks) {
-			System.out.println(row);
-			System.out.println(row.length);
-			// TODO check each row for completion
+		for (final Block[] blocks : myFrozenBlocks) {
+			System.out.println(blocks);
+			System.out.println(blocks.length);
+			// TODO check each row for line completion
 			
 		}
 	}
+	
+	// TODO point getter/setter
 	
 	/**
 	 * Returns the board as a string
@@ -135,18 +157,18 @@ public class BoardModel {
 		final StringBuilder sb = new StringBuilder();
 		// TODO sb.append(function: create ceiling buffer)
 		// for each row
-		for (int i = myHeight; i >= 0; i--) {
+		for (int row = myHeight; row >= 0; row--) {
 			
-			final Block[] row = Objects.requireNonNull(myFrozenBlocks.get(i));
+			final Block[] blocks = Objects.requireNonNull(myFrozenBlocks.get(row));
 			sb.append('|');	// left border
 			
 			// for each column
-			for (int j = 0; j < myWidth - 1; j++) {
-				final Block col = row[j];
-				if (col == null) {
+			for (int col = 0; col < myWidth - 1; col++) {
+				final Block b = blocks[col];
+				if (b == null) {
 					sb.append(' ');	// empty block space on board
 				} else {
-					sb.append(col);
+					sb.append(b);
 				}
 			}	
 			sb.append("|\n");	// start new row
