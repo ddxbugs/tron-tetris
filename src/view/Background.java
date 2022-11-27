74a1a4b1f925c6e5f57d62f1ea4ed747b6ff3eb2
ddxbugs <ddxbugs@proton.me>
2022-11-27 12:12:11 -0800

    view/Background add game label feature

--- src/view/Background.java
+++ src/view/Background.java
@@ -5,7 +5,7 @@
 package view;
 
 import java.awt.Color;
-import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
@@ -13,38 +13,69 @@ import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+
+import res.ColorPalette;
 
 /**
  * Background class displays Tron Legacy images
  */
 public class Background extends JPanel {
-	/**
-	 * Generated serial version UID
-	 */
-	private static final long serialVersionUID = -4143335180036947216L;
+	/** Default serial version UID */
+	private static final long serialVersionUID = 1L;
+	/** Official movie release title */
+	private static final String TRON_LEGACY = "Disney's TRON: Legacy";
+	/** Game title */
+	private static final String VERSION_TITLE = "Super Tetris";
+	/** CrNNtn-regular font family name */
+	private static final String REGULAR = "CRRNTN-Regular";
+	/** CrNNtn font family name */
+	private static final String OUTLINE = "CRRNTN-Outline";
 	
-	private static BufferedImage background;
+	/** Background buffered image */
+	private static BufferedImage myImage;
+	/** Tron Legacy decoration label */
+	private static JLabel myTronLabel;
+	/** Cyan disc menu button decoration label */
+	private static JLabel myGameLabel;
+	/** */
+	private static JLabel myBackgroundLabel;
+	
 	/**
 	 * @param layout
 	 */
 	protected Background() {
 		super();
 		setBackground(Color.BLACK);
+		
 		try {
-			background = ImageIO.read(new File("src/res/grid.jpg"));
-			System.out.println(background.getWidth()+","+background.getHeight());	// TODO debug, uncomment remove me
+			myImage = ImageIO.read(new File("src/res/grid.jpg"));
+			myBackgroundLabel = new JLabel(new ImageIcon(myImage));
 		} catch (IOException e) {
 			System.err.println("IOException:" + e);
 			e.printStackTrace();
 		}
+		
+		myTronLabel = new JLabel(TRON_LEGACY);
+		myGameLabel = new JLabel(VERSION_TITLE);
+	
+		myTronLabel.setFont(new Font(OUTLINE, Font.BOLD, 24));
+		myTronLabel.setForeground(ColorPalette.CYAN_TRON_LEGACY.getColor());
+		myGameLabel.setFont(new Font(REGULAR, Font.TRUETYPE_FONT, 18));
+		myGameLabel.setForeground(ColorPalette.TRON_BLUE.getColor());
+		
+		add(myTronLabel);
+		add(myGameLabel);
+		add(myBackgroundLabel);
 	}
 	
 	public void paintComponents(Graphics theGraphics) {
 		super.paintComponents(theGraphics);
 		final Graphics2D g2d = (Graphics2D) theGraphics;
-		if (background != null)
-			g2d.drawImage(background, background.getWidth(), background.getHeight(), this);
-		System.out.println("background="+background.getWidth()+","+background.getHeight());
+		if (myImage != null)
+			g2d.drawImage(myImage, myImage.getWidth(), myImage.getHeight(), this);
+		System.out.println("background="+myImage.getWidth()+","+myImage.getHeight());
 	}
 }
