d3de79ed628648eef4b9f4bb503efa58a3284811
ddxbugs <ddxbugs@proton.me>
2022-11-26 17:14:44 -0800

    view/ScorePanel initial commit

---
+++ src/view/ScorePanel.java
@@ -0,0 +1,20 @@
+/*
+ * ScorePanel.java
+ * @author ddxbugs 
+ */
+package view;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import model.Player;
+
+/**
+ *	Display the current game score
+ */
+public class ScorePanel extends JPanel {
+	private Player myPlayer;
+	private JLabel myScore;
+	private JLabel myLevel;
+	private JLabel myLines;
+}
