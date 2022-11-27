e148546cbefff50338c2d3fb358a7296f62b74a7
ddxbugs <ddxbugs@proton.me>
2022-11-26 16:12:49 -0800

    view/PreviewPanel iniital commit

---
+++ src/view/PreviewPanel.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package view;
+
+import java.awt.LayoutManager;
+
+import javax.swing.JPanel;
+
+/**
+ * @author User
+ *
+ */
+public class PreviewPanel extends JPanel {
+
+	/**
+	 * 
+	 */
+	public PreviewPanel() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param layout
+	 */
+	public PreviewPanel(LayoutManager layout) {
+		super(layout);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param isDoubleBuffered
+	 */
+	public PreviewPanel(boolean isDoubleBuffered) {
+		super(isDoubleBuffered);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param layout
+	 * @param isDoubleBuffered
+	 */
+	public PreviewPanel(LayoutManager layout, boolean isDoubleBuffered) {
+		super(layout, isDoubleBuffered);
+		// TODO Auto-generated constructor stub
+	}
+
+}
