85051210291f178e570d75f1a28cbef1a9b0e47c
ddxbugs <ddxbugs@proton.me>
2022-11-26 16:50:00 -0800

    view/Background refactor update unused constructor api

--- src/view/Background.java
+++ src/view/Background.java
@@ -6,9 +6,7 @@ package view;
 
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.LayoutManager;
-import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
 
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
@@ -18,8 +16,6 @@ import javax.swing.JPanel;
  * Background class displays Tron Legacy images
  */
 public class Background extends JPanel {
-	private static Image image = 
-			Toolkit.getDefaultToolkit().getImage(".\\src\\res\\background.jpg");
 	private static JLabel label;
 	/**
 	 * Generated serial version UID
@@ -28,29 +24,10 @@ public class Background extends JPanel {
 	/**
 	 * @param layout
 	 */
-	public Background() {
+	protected Background() {
 		super();
 		// TODO Auto-generated constructor stub
 		setBackground(Color.BLACK);
-		label = new JLabel(new ImageIcon(image));
-		add(label);
-	}
-
-	/**
-	 * @param isDoubleBuffered
-	 */
-	public Background(boolean isDoubleBuffered) {
-		super(isDoubleBuffered);
-		// TODO Auto-generated constructor stub
-	}
-
-	/**
-	 * @param layout
-	 * @param isDoubleBuffered
-	 */
-	public Background(LayoutManager layout, boolean isDoubleBuffered) {
-		super(layout, isDoubleBuffered);
-		// TODO Auto-generated constructor stub
 	}
 	
 	public void paintComponents(Graphics g) {
