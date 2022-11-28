5ae2f91da3be7faa04c9c5df43c63cdff9786dd4
ddxbugs <ddxbugs@proton.me>
2022-11-27 20:40:07 -0800

    view/PreviewPanel update background color look and feel

--- src/view/PreviewPanel.java
+++ src/view/PreviewPanel.java
@@ -9,22 +9,27 @@ import java.awt.Graphics2D;
 
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 import javax.swing.border.BevelBorder;
 import javax.swing.border.Border;
+
+import res.ColorPalette;
 
 /**
  * PreviewPanel class displays next tetris piece on board
  */
 public class PreviewPanel extends JPanel {
-	private static final Border bf = BorderFactory.createSoftBevelBorder(BevelBorder.RAISED);
 	/**
-	 * 
+	 * Preview panel class displays next tetris piece during game
 	 */
 	public PreviewPanel() {
 		// TODO Auto-generated constructor stub
 		super();
-		setSize(150, 300);	// TODO debug, remove hard code 
-		setBorder(bf);
+		
+		setLocation(900, 300);
+		setBackground(ColorPalette.MEANWHILE.getColor());
+		setBorder(BorderFactory.createSoftBevelBorder(BevelBorder.RAISED, 
+				ColorPalette.PANE.getColor(), ColorPalette.TRON_BLUE.getColor()));
 //		setVisible(true);
 		
 	}
@@ -34,6 +39,6 @@ public class PreviewPanel extends JPanel {
 		super.paintComponent(theGraphics);
 		final Graphics2D g2d = (Graphics2D) theGraphics;
 		// select tetris piece 
-		// draw
+		// fill and draw the rects
 	}
 }
