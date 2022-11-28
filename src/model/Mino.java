66bb8bfa10a107f596959ec6c6bb36a2df7165b6
ddxbugs <ddxbugs@proton.me>
2022-11-27 21:10:34 -0800

    model/Mino update class name

---
+++ src/model/Mino.java
@@ -0,0 +1,27 @@
+/*
+ * Mino.java
+ * @author Alan Fowler
+ */
+package model;
+
+/**
+ *	The enum type of Tetromino blocks
+ */
+public enum Mino {	
+	/** Empty */
+	EMPTY,
+	/** The 'I' enum block Tetromino  */
+	I,
+	/** The 'J' enum block Tetromino  */
+	J,
+	/** The 'L' enum block Tetromino  */
+	L,
+	/** The 'O' enum block Tetromino  */
+	O,
+	/** The 'S' enum block Tetromino  */
+	S,
+	/** The 'T' enum block Tetromino  */
+	T,
+	/** The 'Z' enum block Tetromino  */
+	Z;
+}
