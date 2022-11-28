73c0381382c1a479ce1d97500190094d0da0f70e
ddxbugs <ddxbugs@proton.me>
2022-11-27 21:09:47 -0800

    model/ImmutableTetromino code refactor class name

--- src/model/ImmutableTetromino.java
+++ src/model/ImmutableTetromino.java
@@ -1,5 +1,5 @@
 /*
- * TetrisPiece.java
+ * Tetromino.java
  * @author Alan Fowler
  */
 package model;
@@ -9,56 +9,56 @@ import java.util.Random;
 
 /**
  * 
- * TetrisPiece Enum type
+ * Tetromino Enum type
  * @author ddxbugs@github.com
  *
  */
-enum ImmutableTetrisPiece {
+enum ImmutableTetromino {
 	
-	/** The 'I' TetrisPiece */
-	I(4, 1, Block.I, 
+	/** The 'I' Tetromino */
+	I(4, 1, Mino.I, 
 			new Point (0, 2),
 			new Point (1, 2),
 			new Point (2, 2),
 			new Point (3, 2)),
 	
-	/** The 'J' TetrisPiece */
-	J(3, 2, Block.J,
+	/** The 'J' Tetromino */
+	J(3, 2, Mino.J,
 			new Point (0, 2),
 			new Point (0, 1),
 			new Point (1, 1),
 			new Point (2, 1)),
 	
-	/** The 'L' TetrisPiece */
-	L(3, 2, Block.L,
+	/** The 'L' Tetromino */
+	L(3, 2, Mino.L,
 			new Point (2, 2),
 			new Point (0, 1),
 			new Point (1, 1),
 			new Point (2, 1)),
 	
-	/** The 'O' TetrisPiece */
-	O(2, 2, Block.O,
+	/** The 'O' Tetromino */
+	O(2, 2, Mino.O,
 			new Point (1, 2),
 			new Point (2, 2),
 			new Point (1, 1),
 			new Point (2, 1)),
 	
-	/** The 'S' TetrisPiece */
-    S(3, 2, Block.S,
+	/** The 'S' Tetromino */
+    S(3, 2, Mino.S,
     		new Point (1, 2), 
 			new Point (2, 2), 
 			new Point (0, 1), 
 			new Point (1, 1)),
     
-    /** The 'T' TetrisPiece */
-    T(3, 2, Block.T,
+    /** The 'T' Tetromino */
+    T(3, 2, Mino.T,
     		new Point (1, 2), 
     		new Point (0, 1), 
     		new Point (1, 1), 
     		new Point (2, 1)),
 
-    /** The 'Z' TetrisPiece */
-    Z(3, 2, Block.Z,
+    /** The 'Z' Tetromino */
+    Z(3, 2, Mino.Z,
     		new Point (0, 2), 
     		new Point (1, 2), 
     		new Point (1, 1), 
@@ -66,21 +66,21 @@ enum ImmutableTetrisPiece {
 	
 	/** The random class object */
 	private static final Random RAND = new Random();
-	/** The block width of the TetrisPiece */
+	/** The block width of the Tetromino */
 	private final int myWidth;
-	/** The block height of the TetrisPiece */
+	/** The block height of the Tetromino */
 	private final int myHeight;
-	/** The TetrisPiece Block Enum type */
-	private final Block myBlock;
-	/** The TetrisPiece points array */
+	/** The Tetromino Mino Enum type */
+	private final Mino myBlock;
+	/** The Tetromino points array */
 	private final Point[] myPoints;
 	/**
-	 * TetrisPiece default constructor
-	 * @param theWidth TetrisPiece block width
-	 * @param theHeight TetrisPiece block height
-	 * @param theBlock TetrisPiece block Enum type
+	 * Tetromino default constructor
+	 * @param theWidth Tetromino block width
+	 * @param theHeight Tetromino block height
+	 * @param theBlock Tetromino block Enum type
 	 */
-	ImmutableTetrisPiece(final int theWidth, final int theHeight, final Block theBlock, final Point... thePoints) {
+	ImmutableTetromino(final int theWidth, final int theHeight, final Mino theBlock, final Point... thePoints) {
 		myWidth = theWidth;
 		myHeight = theHeight;
 		myBlock = theBlock;
@@ -88,42 +88,42 @@ enum ImmutableTetrisPiece {
 	}
 	
 	/**
-	 * Return the TetrisPiece block width
-	 * @return the block width of the TetrisPiece
+	 * Return the Tetromino block width
+	 * @return the block width of the Tetromino
 	 */
 	protected int getWidth() {
 		return myWidth;
 	}
 	
 	/**
-	 * Return the TetrisPiece block height
-	 * @return the block height of the TetrisPiece
+	 * Return the Tetromino block height
+	 * @return the block height of the Tetromino
 	 */
 	protected int getHeight() {
 		return myHeight;
 	}
 	
 	/**
-	 * Return the TetrisPiece Block Enum type
-	 * @return the Block Enum type
+	 * Return the Tetromino Mino Enum type
+	 * @return the Mino Enum type
 	 */
-	protected Block getBlock() {
+	protected Mino getBlock() {
 		return myBlock;
 	}
 	
 	/**
-	 * Return the TetrisPiece point array
-	 * @return the Points of the TetrisPiece
+	 * Return the Tetromino point array
+	 * @return the Points of the Tetromino
 	 */
 	protected Point[] getPoints() {
 		return myPoints.clone();
 	}
 	
 	/**
-	 * Return a random TetrisPiece
-	 * @return a random TetrisPiece
+	 * Return a random Tetromino
+	 * @return a random Tetromino
 	 */
-	protected static ImmutableTetrisPiece getRandomPiece() {
+	protected static ImmutableTetromino getRandomPiece() {
 		return values()[RAND.nextInt(values().length)];
 	}
 }
