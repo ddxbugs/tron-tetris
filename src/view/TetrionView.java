982cb78bf5cb15c5897ceebf43a88dd50ed9f36c
ddxbugs <ddxbugs@proton.me>
2022-12-03 21:17:40 -0800

    view/TetrionView refactor update class key listener and paint component method

--- src/view/TetrionView.java
+++ src/view/TetrionView.java
@@ -20,6 +20,8 @@ import javax.swing.JPanel;
 import javax.swing.Timer;
 import javax.swing.border.BevelBorder;
 
+import controller.GraphicsController;
+import controller.PlayerController;
 import model.Mino;
 import model.TetrionViewModel;
 import res.ColorPalette;
@@ -27,12 +29,12 @@ import res.ColorPalette;
 /**
  * The view model class displays the current board
  */
-public class TetrionView extends JPanel implements ActionListener, KeyListener {
+public class TetrionView extends JPanel implements ActionListener {
 	
 	/** Default serialVersionUID */
 	private static final long serialVersionUID = 1L;
 	/** Set Mino block scale to screen dimension */
-	private static final int SCALE = 1;
+	private static final int SCALE = 0;
 	/** Magic number 1 second delay */
 	private static final int THOUSAND = 1000;
 	
@@ -59,7 +61,8 @@ public class TetrionView extends JPanel implements ActionListener, KeyListener {
 				ColorPalette.PANE.getColor(), ColorPalette.TRON_BLUE.getColor()));
 		setBackground(ColorPalette.MEANWHILE.getColor());
 		
-		addKeyListener(this);
+		addKeyListener(new PlayerController(this));
+//		addFocusListener();	// TODO window focus music, pause listener
 		
 	}
 	/**
@@ -71,62 +74,7 @@ public class TetrionView extends JPanel implements ActionListener, KeyListener {
 		final Graphics2D g2D = (Graphics2D) theGraphics;
 		g2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
 				RenderingHints.VALUE_ANTIALIAS_ON);
-		drawBlocks(g2D);
-	}
-	/**
-	 * Colorizes the individual blocks in the game board 
-	 * @param theGraphics2D Returns corresponding piece color for the block
-	 */
-	private void drawBlocks(final Graphics2D theGraphics2D) {
-		int x = 0, y = 0;	// starting x, y 
-		int w = (int) (getWidth() / SCALE);	// draw rectangle scale to x 
-		int h = (int) (getHeight() / SCALE); // draw rectangle scale to y 
-		
-		// TODO block throw null error, initialized as null "empty"
-		for (Mino block : myModel.getFrozenBlocks()) {
-			
-			if (block == null) block = Mino.EMPTY;	// TODO error block null initialized, but make block final
-			
-			switch(block) {
-			
-			case EMPTY:
-				theGraphics2D.setColor(ColorPalette.MEANWHILE.getColor());
-			case I: 
-				theGraphics2D.setColor(ColorPalette.BASESTAR.getColor()); 
-				break;
-			case J: 
-				theGraphics2D.setColor(ColorPalette.PANE.getColor()); 
-				break;
-			case L: theGraphics2D.setColor(ColorPalette.SWEET_YELLOW.getColor()); 
-				break;
-			case O: 
-				theGraphics2D.setColor(ColorPalette.FAR_AWAY_SKY.getColor()); 
-				break;
-			case S: 
-				theGraphics2D.setColor(ColorPalette.DEAD_BABY.getColor()); 
-				break;
-			case T: 
-				theGraphics2D.setColor(ColorPalette.TRON_BLUE.getColor()); 
-				break;
-			case Z: 
-				theGraphics2D.setColor(ColorPalette.WIPE_OUT.getColor()); 
-				break;
-			default: 
-				theGraphics2D.setColor(Color.RED);	// Debug, remove me
-				break;
-			}
-			
-			final Rectangle2D r = new Rectangle(w, y, w, h);
-			theGraphics2D.fill(r);
-			theGraphics2D.draw(r);
-			x += w;
-			// reset column counter, increment next row counter
-			if (x % w == 0) {
-				x = 0;	
-				y += h;
-			}
-		}
-		
+		GraphicsController.drawBlocks(g2D, myModel.getFrozenBlocks(), 0, 0, 0, 0);
 	}
 	
 	@Override
@@ -135,26 +83,5 @@ public class TetrionView extends JPanel implements ActionListener, KeyListener {
 		myModel.down();
 //		repaint();
 //		System.out.println(myModel.toString());
-	}
-	
-	@Override
-	public void keyTyped(KeyEvent e) {
-//		System.out.println("KEY_TYPED: " + e);		
-	}
-	@Override
-	public void keyPressed(KeyEvent e) {
-//		System.out.println("KEY_PRESSED: " + e);
-		if (e.getKeyChar() == KeyEvent.VK_ENTER) {
-			myModel.newGame();
-			myTimer.start();
-		} else {
-			myTimer.stop();
-		}
-		
-	}
-	@Override
-	public void keyReleased(KeyEvent e) {
-//		System.out.println("KEY_RELEASED: " + e);
-		
 	}
 }
