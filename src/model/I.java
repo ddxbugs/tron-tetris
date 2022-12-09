abb6e1b2a36a801029f4771a6612054f0564fb82
ddxbugs <ddxbugs@proton.me>
2022-12-08 19:05:54 -0800

    model/I tetromino initial commit

---
+++ src/model/I.java
@@ -0,0 +1,15 @@
+package model;
+
+public class I extends Tetromino {
+
+	public I(ImmutableTetromino theTetromino, Point thePosition, Rotation theRotation) {
+		super(theTetromino, thePosition, theRotation);
+		// TODO Auto-generated constructor stub
+	}
+
+	public I(ImmutableTetromino theTetromino, Point thePosition) {
+		super(theTetromino, thePosition);
+		// TODO Auto-generated constructor stub
+	}
+
+}
