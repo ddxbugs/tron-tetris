9dea21bdbec0b03b77f910ebaf89ea8a6032e424
ddxbugs <ddxbugs@proton.me>
2022-12-13 16:07:15 -0800

    controller/TetrionController viewmodel intitial commit

---
+++ src/controller/TetrionController.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package controller;
+
+import model.TetrionViewModel;
+import view.TetrionView;
+
+/**
+ * @author User
+ *
+ */
+public class TetrionController {
+	private TetrionView view;
+	private TetrionViewModel model;
+	
+	/**
+	 * 
+	 */
+	public TetrionController() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
