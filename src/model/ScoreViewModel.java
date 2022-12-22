96adbe38e94409aee2a85323b772e50b804d0203
ddxbugs <ddxbugs@proton.me>
2022-12-22 02:20:09 -0800

    model/ScoreViewModel.java

--- src/model/ScoreViewModel.java
+++ src/model/ScoreViewModel.java
@@ -1,12 +1,43 @@
-/**
- * 
+/*
+ * @author ddxbugs
  */
 package model;
 
 /**
- * @author User
- *
+ * Score view model class
  */
 public class ScoreViewModel {
-
+	private int myCurrentScore;
+	private int myCurrentLevel;
+	private int myCurrentLines;
+	
+	/**
+	 * Initial default public view model constructor
+	 */
+	public ScoreViewModel() {
+		myCurrentScore = 0;
+		myCurrentLevel = 0;
+		myCurrentLines = 0;
+	}
+	/**
+	 * The player current score
+	 * @return The current score
+	 */
+	public int getScore() {
+		return myCurrentScore;
+	}
+	/**
+	 * The player current level
+	 * @return The current level
+	 */
+	public int getLevel() {
+		return myCurrentLevel;
+	}
+	/**
+	 * The player current lines 
+	 * @return The current number of lines completed
+	 */
+	public int getLines() {
+		return myCurrentLines;
+	}
 }
