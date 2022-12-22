72f4b7b0459e08b3731b5f6127515284ab6d2b1e
ddxbugs <ddxbugs@proton.me>
2022-12-21 22:33:16 -0800

    model/ConfigViewModel mv model/MenuViewModel refactor for code readability

---
+++ src/model/MenuViewModel.java
@@ -0,0 +1,16 @@
+/*
+ * MenuOptionView.java
+ * @author ddxbugs 
+ */
+package model;
+
+/**
+ *	Game menu settings and configurations view model 
+ */
+public class MenuViewModel {
+	// class field 
+	
+	public MenuViewModel() {
+		
+	}
+}
