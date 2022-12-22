e13355ed43fdb1489d5496f49a72116efd452c89
ddxbugs <ddxbugs@proton.me>
2022-12-22 02:17:56 -0800

    model/Player persistence player stat tracking

--- src/model/Player.java
+++ src/model/Player.java
@@ -8,37 +8,40 @@ package model;
  * Persistence player class model 
  */
 public class Player {
+	// TODO String pattern regex 0-9azAZ!@#$%^&*() and duplicates
+	/** Max three character initial */
+	private int INITIALS_ARR_SIZE = 3;
 	/** Player game level difficulty */
 	private int myCurrentLevel;
 	/** Player highest score */
 	private int myHighestScore;
+	/** Player total lines */
+	private int myLines;
 	/** Player end game initials */
 	private char[] myInitials;
 	/**
-	 * protected class constructor
+	 * protected player default class constructor
 	 */
 	public Player() {
-		// TODO Auto-generated constructor stub
 		myCurrentLevel = 0;
 		myHighestScore = 0;
-		myInitials = new char[3];
+		myLines = 0;
+		myInitials = new char[INITIALS_ARR_SIZE];
 	}
-	
 	/**
 	 * Returns Player current game level
-	 * @return Player level 0-99
+	 * @return Player level 0-999
 	 */
-	protected int getLevel() {
+	public int getLevel() {
 		return myCurrentLevel;
 	}
 	/** 
 	 * Increment the Player level
-	 * Increases game speed difficulty
 	 */
-	protected void levelUp() {
+	public void levelUp() {
 		myCurrentLevel++;
 	}
-	
+
 	/**
 	 * Return the highest score in saved game
 	 * @return The highest score earned
@@ -46,7 +49,20 @@ public class Player {
 	protected int getHighestScore() {
 		return myHighestScore;
 	}
-	
+	/**
+	 * Set player high score
+	 * @param theHighestScore The highest score
+	 */
+	protected void setHighestScore(final int theHighestScore) {
+		myHighestScore = theHighestScore;
+	}
+	/**
+	 * Return the number of lines completed
+	 * @return The number of lines completed
+	 */
+	protected int getLines() {
+		return myLines;
+	}
 	/**
 	 * Return the three letter initials
 	 * @return The player post game initials
@@ -54,4 +70,15 @@ public class Player {
 	protected String getInitials() {
 		return myInitials.toString().toUpperCase();
 	}
+	/**
+	 * Save persistent player initials
+	 * @param theInitials Player initials
+	 */
+	protected void setInitials(final char[] theInitials) {
+		char[] initials = new char[INITIALS_ARR_SIZE];
+		for (int i = 0; i < initials.length; i++) {
+			initials[i] = theInitials[i];
+		}
+	}
+	
 }
