e4382acc2dbbffc4bcb1fd043786a975bac3435f
ddxbugs <ddxbugs@proton.me>
2022-12-23 00:10:38 -0800

    controller/TetrionController update class constructor api interface

--- src/controller/TetrionController.java
+++ src/controller/TetrionController.java
@@ -5,7 +5,7 @@ package controller;
 
 import model.Player;
 import model.TetrionViewModel;
-import view.PiecePreview;
+import view.Preview;
 import view.ScoreView;
 import view.TetrionView;
 
@@ -21,48 +21,34 @@ public class TetrionController {
 	private TetrionViewModel model;
 	
 	/** Next Tetromino preview window */
-	private PiecePreview preview;
+	private Preview preview;
 	/** Current game score tracker */
 	private ScoreView score;
 	/** Persistent player game model */;
 	private Player player;
 	
 	/** 
-	 * Primary class constructor 
+	 * Primary public class constructor 
 	 */
-	public TetrionController(final TetrionView View, final PiecePreview Preview, final ScoreView Score) {
+	public TetrionController(final TetrionView View, final Preview Preview, final ScoreView Score) {
 		view = View;	// tetris view panel
 		preview = Preview;	// tetromino preview panel
 		score = Score;	// score level lines view label
 		player = null;	//	persistent player model
+		
+		// TODO add listener 
+		view.addPropertyChangeListener(STRING, preview);
+		view.addPropertyChangeListener(STRING, score);
 	
 		// TODO implement action event listener methods: timer, next piece, update score
 		// TODO implement property change event listener methods: update border and text effects
 	}
 	
 	/** 
-	 * Default public constructor 
+	 * Private constructor prevent instantiation 
 	 */
-	public TetrionController(final int theWidth, final int theHeight) {
-		view = new TetrionView();	// tetris view panel
-		preview = new PiecePreview();	// tetromino preview panel
-		score = new ScoreView();	// score level lines
-		player = new Player();	// default player model
-		
-		setUpComponents();
-	}
-	
-	/**
-	 * Set view component properties
-	 */
-	private void setUpComponents() {
-		view.setSize(300, 600);
-		preview.setSize(200, 200);	// small window
-		score.setSize(400, 100);	// transparent row line
-		
-		view.addPropertyChangeListener(STRING, preview);
-		view.addPropertyChangeListener(STRING, score);
-		
+	private TetrionController() {
+		throw new IllegalArgumentException();
 	}
 
 	private void updateScore() {
