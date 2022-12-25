b59ff3e3adc6e6a40cd48ff81aebb90030b57c2c
ddxbugs <ddxbugs@proton.me>
2022-12-25 11:33:08 -0800

    controller/MenuController update view component constructor api

--- src/controller/MenuController.java
+++ src/controller/MenuController.java
@@ -6,25 +6,25 @@ package controller;
 
 import model.MenuViewModel;
 import view.MenuView;
-import view.Tetrion;
+import view.TetrionGrid;
 
 /**
  */
 public class MenuController {
-	private static final int MAX_SCREEN_WIDTH = 0;
-	private static final int MAX_SCREEN_HEIGHT = 0;
-	private static final String CONFIG_FILE = "config.ini";
-	private static Tetrion tetrion;
+//	private static final String CONFIG_FILE = "config.ini";
+	
+	/** The TetrionGrid view */
+	private static TetrionGrid grid;
 	/** Configuration settings menu option */
 	private static MenuView menu;
 	private static MenuViewModel model;
 	/** 
 	 * Primary menu controller class constructor
-	 * @param theTetrion The tetrion 
+	 * @param theGrid The grid 
 	 * @param theMenu The options menu
 	 */
-	public MenuController(final Tetrion theTetrion, final MenuView theMenu) {
-		tetrion = theTetrion;
+	public MenuController(final TetrionGrid theGrid, final MenuView theMenu) {
+		grid = theGrid;
 		menu = theMenu;
 	}
 	/**
@@ -34,7 +34,7 @@ public class MenuController {
 		// TODO Auto-generated constructor stub
 		super();
 		menu = new MenuView();	// game options menu
-		tetrion = new Tetrion();
+		grid = new TetrionGrid();
 		model = new MenuViewModel();
 		
 		menu.setSize(500,500);
